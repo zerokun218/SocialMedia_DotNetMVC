a4616f5510e09f8da26f0c0a926f37541bcb4b21
zerokun218 <hoandv18@uef.edu.vn>
2021-06-28 22:44:53 +0700

    first commit

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialMedia.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+        public RoleController() {
+            context = new ApplicationDbContext();
+        }
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            roleManager.Create(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialMedia.Controllers
+{
+    //[Authorize]
+    public class UsersController : Controller
+    {
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                var userList = new ApplicationDbContext().Users.ToList();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                List<string> roleList = new List<string>();
+                foreach (var u in userList) {
+                    if (UserManager.GetRoles(u.Id) != null)
+                    {
+                        roleList.Add(UserManager.GetRoles(u.Id)[0].ToString());
+                        System.Diagnostics.Debug.WriteLine(UserManager.GetRoles(u.Id)[0].ToString());
+                    }
+                    else {
+                        roleList.Add("null");
+                    }
+                    
+                }
+                ViewBag.RoleList = roleList;
+                return View(userList);
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+        }
+    }
+}
